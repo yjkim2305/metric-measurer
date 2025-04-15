95aef9277ff2ae61f83de02e222625f8553e09a1
cyber <cyberact3181@gmail.com>
2025-04-15 17:26:51 +0900

    feat: add MetricTypes

--- src/main/java/io/ten1010/gpumetricmonitor/domain/enums/MetricType.java
+++ src/main/java/io/ten1010/gpumetricmonitor/domain/enums/MetricType.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum MetricType {
     GPU_UTIL("DCGM_FI_DEV_GPU_UTIL", "GPU 사용률"),
     GPU_TEMP("DCGM_FI_DEV_GPU_TEMP", "GPU 온도"),
-    MEMORY_TEMP("DCGM_FI_DEV_MEMORY_TEMP", "메모리 온도")
+    MEMORY_TEMP("DCGM_FI_DEV_MEMORY_TEMP", "메모리 온도"),
+    POWER_USAGE("DCGM_FI_DEV_POWER_USAGE", "GPU 전력소비"),
+    GPU_CLOCK("DCGM_FI_DEV_GPU_CLOCK", "GPU 클럭속도")
     ;
 
     private final String metricName;
