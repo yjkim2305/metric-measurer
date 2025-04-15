5015572de0693771a464c1ed4bdd2e72a9e1581b
cyber <cyberact3181@gmail.com>
2025-04-15 16:29:07 +0900

    feat: add MetricType enum

---
+++ src/main/java/io/ten1010/gpumetricmonitor/domain/enums/MetricType.java
@@ -0,0 +1,19 @@
+package io.ten1010.gpumetricmonitor.domain.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum MetricType {
+    GPU_UTIL("DCGM_FI_DEV_GPU_UTIL", "GPU 사용률"),
+    GPU_TEMP("DCGM_FI_DEV_GPU_TEMP", "GPU 온도"),
+    MEMORY_TEMP("DCGM_FI_DEV_MEMORY_TEMP", "메모리 온도")
+    ;
+
+    private final String metricName;
+    private final String label;
+
+    MetricType(String metricName, String label) {
+        this.metricName = metricName;
+        this.label = label;
+    }
+}
