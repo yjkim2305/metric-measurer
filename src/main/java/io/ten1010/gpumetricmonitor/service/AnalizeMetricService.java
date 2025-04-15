2221228cffd93d0074cb77e5668b81f8bfcb62a2
cyber <cyberact3181@gmail.com>
2025-04-15 17:29:29 +0900

    feat: metric warning using threshold

--- src/main/java/io/ten1010/gpumetricmonitor/service/AnalizeMetricService.java
+++ src/main/java/io/ten1010/gpumetricmonitor/service/AnalizeMetricService.java
@@ -3,9 +3,12 @@ package io.ten1010.gpumetricmonitor.service;
 
 import io.ten1010.gpumetricmonitor.common.util.TimeUtils;
 import io.ten1010.gpumetricmonitor.domain.DcgmMetric;
+import io.ten1010.gpumetricmonitor.domain.GpuThreshold;
+import io.ten1010.gpumetricmonitor.domain.PrometheusQuery;
+import io.ten1010.gpumetricmonitor.domain.ThresholdQuery;
+import io.ten1010.gpumetricmonitor.domain.enums.MetricType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
@@ -21,37 +24,42 @@ import java.util.List;
 public class AnalizeMetricService implements ApplicationRunner {
     private final PrometheusService prometheusService;
 
-    @Value("${prometheus.query.range}")
-    private String queryRange;
-
-    public Mono<Void> analizeMetrics(String start, String end) {
+    public Mono<Void> analizeThresholdMetrics(String start, String end, GpuThreshold gpuThreshold) {
         long startTime = TimeUtils.convertStartTimestamp(start);
         long endTime = TimeUtils.convertEndTimestamp(end);
 
-        log.debug("Start: {}, End: {}", startTime, endTime);
+        log.info("Start: {}, End: {}", startTime, endTime);
 
-        //GPU 평균 사용률
-        String GPU_UTIL_QUERY = "avg(avg_over_time(DCGM_FI_DEV_GPU_UTIL[" + queryRange + "])) by(gpu, instance)";
-        //GPU 평균 온도
-        String GPU_TEMP_QUERY = "avg(avg_over_time(DCGM_FI_DEV_GPU_TEMP[" + queryRange + "])) by(gpu, instance)";
-        //메모리 평균 온도
-        String MEM_TEMP_QUERY = "avg(avg_over_time(DCGM_FI_DEV_MEMORY_TEMP[" + queryRange + "])) by(gpu, instance)";
+        List<PrometheusQuery> prometheusQueryList = List.of(
+                ThresholdQuery.createThresholdQuery(MetricType.GPU_UTIL.getMetricName(), gpuThreshold.getGpuUtilThreshold(), MetricType.GPU_UTIL.getLabel())
+                , ThresholdQuery.createThresholdQuery(MetricType.GPU_TEMP.getMetricName(), gpuThreshold.getGpuTempThreshold(), MetricType.GPU_TEMP.getLabel())
+                , ThresholdQuery.createThresholdQuery(MetricType.MEMORY_TEMP.getMetricName(), gpuThreshold.getGpuUtilThreshold(), MetricType.MEMORY_TEMP.getLabel())
+        );
 
-        List<String> queryList = List.of(GPU_UTIL_QUERY, GPU_TEMP_QUERY, MEM_TEMP_QUERY);
-
-        return prometheusService.fetchAllMetrics(queryList, startTime, endTime)
+        return prometheusService.fetchAllMetrics(prometheusQueryList, startTime, endTime)
                 .doOnSuccess(DcgmMetric::print)
                 .then();
     }
 
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            //사용자로부터 설정된 임계치 값
+            GpuThreshold gpuThreshold = GpuThreshold.createThreshold(
+                    90, "이상",
+                    50, "이하",
+                    0, "초과");
+
             log.info("시작 날짜를 입력해주세요(yyyymmdd)");
             String startTime = br.readLine();
             log.info("끝 날짜를 입력해주세요(yyyymmdd)");
             String endTime = br.readLine();
-            analizeMetrics(startTime, endTime).block();
+            analizeThresholdMetrics(startTime, endTime, gpuThreshold).block();
         }
     }
+
+
+
+
 }
