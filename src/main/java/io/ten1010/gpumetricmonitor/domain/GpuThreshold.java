fa333c1a94ed53adc0a8ffb6716ca316b721ed31
cyber <cyberact3181@gmail.com>
2025-04-15 17:28:29 +0900

    feat: add CompareType and metric value

--- src/main/java/io/ten1010/gpumetricmonitor/domain/GpuThreshold.java
+++ src/main/java/io/ten1010/gpumetricmonitor/domain/GpuThreshold.java
@@ -1,5 +1,6 @@
 package io.ten1010.gpumetricmonitor.domain;
 
+import io.ten1010.gpumetricmonitor.domain.enums.CompareType;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,23 +9,27 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GpuThreshold {
-    private double gpuUtilThreshold;
-    private double gpuTempThreshold;
-    private double memTempThreshold;
+    private CompareMetricValue gpuUtilThreshold;
+    private CompareMetricValue gpuTempThreshold;
+    private CompareMetricValue memTempThreshold;
 
     @Builder
-    private GpuThreshold(double gpuUtilThreshold, double gpuTempThreshold, double memTempThreshold) {
+    private GpuThreshold(CompareMetricValue gpuUtilThreshold, CompareMetricValue gpuTempThreshold, CompareMetricValue memTempThreshold) {
         this.gpuUtilThreshold = gpuUtilThreshold;
         this.gpuTempThreshold = gpuTempThreshold;
         this.memTempThreshold = memTempThreshold;
     }
 
 
-    public static GpuThreshold createThreshold(double gpuUtilThreshold, double gpuTempThreshold, double memTempThreshold) {
+    public static GpuThreshold createThreshold(
+            double gpuUtilThreshold, String gpuUtilThresholdCompare
+            , double gpuTempThreshold, String gpuTempThresholdCompare
+            , double memTempThreshold, String memTempThresholdCompare) {
+
         return GpuThreshold.builder()
-                .gpuUtilThreshold(gpuUtilThreshold)
-                .gpuTempThreshold(gpuTempThreshold)
-                .memTempThreshold(memTempThreshold)
+                .gpuUtilThreshold(CompareMetricValue.create(gpuUtilThreshold, CompareType.fromByCmpareValue(gpuUtilThresholdCompare)))
+                .gpuTempThreshold(CompareMetricValue.create(gpuTempThreshold, CompareType.fromByCmpareValue(gpuTempThresholdCompare)))
+                .memTempThreshold(CompareMetricValue.create(memTempThreshold, CompareType.fromByCmpareValue(memTempThresholdCompare)))
                 .build();
     }
 }
